c92693f717857fff0b1deaafed4b2c71b9ba3398
Diegogtz03 <diego_gtz_t@hotmail.com>
2023-07-13 23:42:05 +0000

    Update redploy-website script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Kill all tmux sessions
-
-tmux has-session -t "portfolio" 2>/dev/null
-if [ $? != 1 ]; then tmux kill-server; fi
-
 # cd into projects folder
 cd /root/MLH-Portfolio
 
@@ -17,7 +12,8 @@ source python3-virtualenv/bin/activate
 # install dependencies
 pip install -r requirements.txt >/dev/null
 
-# start new tmux session and start flask server
-tmux new -s portfolio -d 'flask run --host=0.0.0.0'
+# reload service and restart 'myportfolio' service
+systemctl daemon-reload
+system restart myportfolio
 
 echo "Website Re-deployed!"

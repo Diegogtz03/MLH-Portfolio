5d5c96407bc62c13b70701b8dfb6d7f852b7e2ce
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-07-16 21:21:24 -0500

    📸 Added project photos

--- app/__init__.py
+++ app/__init__.py
@@ -49,55 +49,57 @@ def projects():
         {
             "id": "1",
             "title": "GT Dent",
-            "image": "../static/projects/project1.jpg",
+            "image": "../static/projects/GT_Dent.png",
             "description": "Java desk application designed for the registry and management of dental patient records and prescription creation in a dental office. This greatly reduced friction in the office with 1000+ records registered while saving up to 90% of the time looking for and updating the records. Database information exchanges handled with MySQL. Created also using dropbox API, java PDF creator and Figma.",
         },
         {
             "id": "2",
             "title": "MediFAST",
-            "image": "../static/projects/project1.jpg",
-            "description": "Web application combined with a physical pill dispenser that provides a safe way for doctors and families to give out prescriptions for patients with disabilities. It was built using a combination of Arduino, NodeMCU, C++, Python, Flask and HTML. Worked in a team of 4, personally helped in putting together the website and programmed the logic for the pill dispenser using a NodeMCU.",
+            "image": "../static/projects/Medifast.png",
+            "description": "Web application combined with a physical pill dispenser that provides a safe way for doctors and families to give out prescriptions for patients with disabilities. This system also helped in the generation and validation of medical prescriptions to reduce medication robbery around the world. It was built using a combination of Arduino, NodeMCU, C++, Python, Flask and HTML. Worked in a team of 4, personally helped in putting together the website and programmed the logic for the pill dispenser using a NodeMCU. We were ranked at the top 10 places in the hackathon!",
+            "link": "https://devpost.com/software/medifast-bzme2h" 
         },
         {
             "id": "3",
             "title": "Trainees Managment System",
-            "image": "../static/projects/project1.jpg",
+            "image": "../static/projects/traineeSystem.png",
             "description": "A full-stack web application built for a company to help guide their traineers in their learning process while helping the admins take better administration of the program. This website included a game made specifically to motivate the trainees to continue the program. The system was built using React, Next, SQL, .NET, and Unity. Personally worked on the whole front-end and part of the back-end of the website.",
         },
         {
             "id": "4",
             "title": "Med-Link",
-            "image": "../static/projects/project1.jpg",
+            "image": "../static/projects/MedLink.jpeg",
             "description": "Website designed for the global registry and availability of medical records and insurance in emergency situations using facial recognition as a tool for a quick recognition of patients. Built using OpenCV, Python, Flask, HTML, CSS and JS. Personally worked on building the website and setting up the facial recognition.",
+            "link": "https://devpost.com/software/med-link"
         },
         {
             "id": "5",
             "title": "Y2-Hub",
-            "image": "../static/projects/project1.jpg",
+            "image": "../static/projects/Y2Hub.jpeg",
             "description": "Small and simple Python desktop app designed to download videos and audios from Youtube. Made as a learning experience and not as a final product.",
         },
         {
             "id": "6",
             "title": "Kronos - Music Bot",
-            "image": "../static/projects/project1.jpg",
+            "image": "../static/projects/Kronos.png",
             "description": "A personal musical discord bot made using discord's API, Python, Youtube_dl and spotifybot. It allows to play music from youtube and spotify, some of the features include playing a specific song, skipping songs, stopping songs and queueing songs/playlists. It was a fun challenge to get the audio compression, threading and async functions right in order to get the best quality and experience out of the bot!",
         },
         {
             "id": "7",
             "title": "Portfolio Website",
-            "image": "../static/projects/project1.jpg",
+            "image": "../static/projects/Portfolio.png",
             "description": "This very website currently built using Flask, MySQL, HTML, CSS and JS. In the process of migrating the website to React, Next and Three.js.",
         },
         {
             "id": "8",
             "title": "Code Syntax Highlighter",
-            "image": "../static/projects/project1.jpg",
+            "image": "../static/projects/Resaltador.png",
             "description": "A simple console application made using C++ and Regex that allows to highlight the syntax of a code file written in C++. The program is able to create an HTML file highlighting the syntax of the code file with different colors.",
         },
         {
             "id": "9",
             "title": "Calendar Reminder System",
-            "image": "../static/projects/project1.jpg",
+            "image": "../static/projects/Calendar_reminder.png",
             "description": "A web app built to reduce work in a dental office by reminding patients directly in their emails using Google's Calendar API instead of having to call them. Built using Flask, HTML, CSS and JS.",
         }
     ]

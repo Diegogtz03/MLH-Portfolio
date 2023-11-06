e31259bc7f3dd0b67d087c78640670db22b5bd53
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-11-05 22:15:19 -0600

    Add new images and update CSS styles

--- app/__init__.py
+++ app/__init__.py
@@ -60,50 +60,65 @@ def projects():
         },
         {
             "id": "2",
+            "title": "Kofy",
+            "image": "../static/projects/Kofy.png",
+            "description": "Kofy is an iOS app designed with deaf people in mind. It allows to brige the communication gap between deaf people and healthcare professionals by providing easy speech to text and text to speech translations. These conversations get simplified using generative AI and corrected by doctor himself. The app also alows the explanation of prescriptions using vision libraries and the creation of quick reminders. Finally, it allows people to quickly scan medical instruments, and with the help of machine learning, provide quick instructions of their use in sign language. Built using Swift, SwiftUI, CoreML, AVFoundation, and CoreML.",
+            "link": "https://github.com/Diegogtz03/kofy-app.git" 
+        },
+        {
+            "id": "3",
+            "title": "Code Genie",
+            "image": "../static/projects/CodeGenie.png",
+            "description": "We created a website that revolutionizes the way programmers practice and expand their coding skills. Using AI, we developed a personalized code trainer that generates challenges and coding questions based on a programmer's experience, including their previous code snippets and resumes.",
+            "link": "https://devpost.com/software/code-genie#updates" 
+        },
+        {
+            "id": "4",
             "title": "MediFAST",
             "image": "../static/projects/Medifast.png",
             "description": "Web application combined with a physical pill dispenser that provides a safe way for doctors and families to give out prescriptions for patients with disabilities. This system also helped in the generation and validation of medical prescriptions to reduce medication robbery around the world. It was built using a combination of Arduino, NodeMCU, C++, Python, Flask and HTML. Worked in a team of 4, personally helped in putting together the website and programmed the logic for the pill dispenser using a NodeMCU. We were ranked at the top 10 places in the hackathon!",
             "link": "https://devpost.com/software/medifast-bzme2h" 
         },
         {
-            "id": "3",
-            "title": "Trainee managment system",
+            "id": "5",
+            "title": "Trainee management system",
             "image": "../static/projects/traineeSystem.png",
             "description": "A full-stack web application built for a company to help guide their traineers in their learning process while helping the admins take better administration of the program. This website included a game made specifically to motivate the trainees to continue the program. The system was built using React, Next, SQL, .NET, and Unity. Personally worked on the whole front-end and part of the back-end of the website.",
         },
         {
-            "id": "4",
+            "id": "6",
             "title": "Med-Link",
             "image": "../static/projects/MedLink.jpeg",
             "description": "Website designed for the global registry and availability of medical records and insurance in emergency situations using facial recognition as a tool for a quick recognition of patients. Built using OpenCV, Python, Flask, HTML, CSS and JS. Personally worked on building the website and setting up the facial recognition.",
             "link": "https://devpost.com/software/med-link"
         },
         {
-            "id": "5",
+            "id": "7",
             "title": "Y2-Hub",
             "image": "../static/projects/pyhub.png",
             "description": "Small and simple Python desktop app designed to download videos and audios from Youtube. Made as a learning experience and not as a final product.",
         },
         {
-            "id": "6",
+            "id": "8",
             "title": "Kronos - Music Bot",
             "image": "../static/projects/Kronos.png",
             "description": "A personal musical discord bot made using discord's API, Python, Youtube_dl and spotifybot. It allows to play music from youtube and spotify, some of the features include playing a specific song, skipping songs, stopping songs and queueing songs/playlists. It was a fun challenge to get the audio compression, threading and async functions right in order to get the best quality and experience out of the bot!",
         },
         {
-            "id": "7",
+            "id": "9",
             "title": "Portfolio Website",
             "image": "../static/projects/Portfolio.png",
             "description": "This very website currently built using Flask, MySQL, HTML, CSS and JS. In the process of migrating the website to React, Next and Three.js.",
+            "link": "https://github.com/Diegogtz03/portfolio-diegogtzt.git"
         },
         {
-            "id": "8",
+            "id": "10",
             "title": "Code Syntax Highlighter",
             "image": "../static/projects/Resaltador.png",
             "description": "A simple console application made using C++ and Regex that allows to highlight the syntax of a code file written in C++. The program is able to create an HTML file highlighting the syntax of the code file with different colors.",
         },
         {
-            "id": "9",
+            "id": "11",
             "title": "Calendar Reminder System",
             "image": "../static/projects/Calendar_reminder.png",
             "description": "A web app built to reduce work in a dental office by reminding patients directly in their emails using Google's Calendar API instead of having to call them. Built using Flask, HTML, CSS and JS.",
@@ -111,7 +126,6 @@ def projects():
     ]
 
     return render_template('projects.html', projects = projects)
-
 @app.route('/experience')
 def experience():
     experiences = [
@@ -121,6 +135,11 @@ def experience():
             "end_date": "Dec 2025",
         },
         {
+            "company_institution": "Hackathon Top 10: HackMTY",
+            "start_date": "Aug 2021",
+            "end_date": "Aug 2021",
+        },
+        {
             "company_institution": "Web Course Development @ CVA",
             "start_date": "Jul 2022",
             "end_date": "Jun 2023",
@@ -128,6 +147,11 @@ def experience():
         {
             "company_institution": "MLH PE Fellowship",
             "start_date": "Jun 2023",
+            "end_date": "Sep 2023",
+        },
+        {
+            "company_institution": "Hackathon Top 10: HackMTY",
+            "start_date": "Sep 2023",
             "end_date": "Sep 2023",
         }
     ]
